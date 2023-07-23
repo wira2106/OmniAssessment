41bd72f9cd2664d56a2e3ab895d6a16597890604
Putra Wiranata <putrawiranata@Putras-MacBook-Air.local>
2023-07-23 23:08:57 +0800

    testing unit test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\User;
 use App\Repositories\Interfaces\UserRepository;
 use Illuminate\Foundation\Testing\WithFaker;
+use Faker\Factory as Faker;
 use Tests\TestCase;
 
 class UserTest extends TestCase
@@ -45,10 +46,28 @@ class UserTest extends TestCase
     {
 
         $user = User::find(1);
-
+        $faker = Faker::create('id_ID');
+        $firstName = $faker->firstName;
+            $lastName = $faker->lastName;
+            $name = $firstName . " " . $lastName;
+            $email = strtolower($firstName . $lastName) . '@gmail.com';
+            $address = $faker->randomElement([
+                'Jl. Kenangan, Gang Hujan',
+                'Jl. Sudirman, Gang Kenangan',
+                'Jl. Gajah Mada, Gang I',
+                'Jl. Mawar Merah, Gang II',
+                'Jl. Kebo Iwa , Gang II'
+            ]);
+        $data = [
+            'name' => $name,
+            'email' =>  $email,
+            'password' => '12345678',
+            'password_confirmation' => '12345678',
+            'alamat' => $address
+        ];
         $response = $this->actingAs($user)
                          ->withSession(['banned' => false])
-                         ->get('/api/users');
+                         ->post('/api/user/create',$data);
 
         $response->assertStatus(200);
         
@@ -60,10 +79,16 @@ class UserTest extends TestCase
     {
 
         $user = User::find(1);
+        $user_update= $user;
+        $data = [
+                    'name' => $user_update->name."1",
+                    'email' => $user_update->email,
+                    'alamat' => $user_update->alamat,
+                ];
 
         $response = $this->actingAs($user)
                          ->withSession(['banned' => false])
-                         ->get('/api/users');
+                         ->post('/api/user/update/'.$user_update->id,$data);
 
         $response->assertStatus(200);
         
