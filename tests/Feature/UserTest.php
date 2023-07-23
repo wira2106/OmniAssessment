86c64904da1dafab868a719db2eb6d1582a51bb5
Putra Wiranata <putrawiranata@Putras-MacBook-Air.local>
2023-07-23 19:43:16 +0800

    testing unit test user

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,23 +2,24 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use App\Repositories\Interfaces\UserRepository;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    use RefreshDatabase;
     /**
      * @test
      */
     public function user_can_see_list_user_page()
     {
-        $token = "e081ba3d516c2d84ee33d6ad0d9490c7200fe7503daf2b91c2ecdac590dcba6c";
-        $response = $this->get('/user')->withHeaders([
-            'Authorization' => 'Bearer '. $token,
-        ]);
-        
+        $user = User::find(1);
+ 
+        $response = $this->actingAs($user)
+                         ->withSession(['banned' => false])
+                         ->get('/user');
+
         $response->assertStatus(200);
         $response->assertViewIs('users.index');
     }
@@ -27,11 +28,43 @@ class UserTest extends TestCase
      */
     public function user_can_see_get_data_user()
     {
-        $token = "e081ba3d516c2d84ee33d6ad0d9490c7200fe7503daf2b91c2ecdac590dcba6c";
-        $response = $this->json('get', '/api/users')->withHeaders([
-            'Authorization' => 'Bearer '. $token,
-        ]);;
-        dd($response);
+
+        $user = User::find(1);
+
+        $response = $this->actingAs($user)
+                         ->withSession(['banned' => false])
+                         ->get('/api/users');
+
+        $response->assertStatus(200);
+        
+    }
+    /**
+     * @test
+     */
+    public function user_can_create_data_user()
+    {
+
+        $user = User::find(1);
+
+        $response = $this->actingAs($user)
+                         ->withSession(['banned' => false])
+                         ->get('/api/users');
+
+        $response->assertStatus(200);
+        
+    }
+    /**
+     * @test
+     */
+    public function user_can_edit_data_user()
+    {
+
+        $user = User::find(1);
+
+        $response = $this->actingAs($user)
+                         ->withSession(['banned' => false])
+                         ->get('/api/users');
+
         $response->assertStatus(200);
         
     }
